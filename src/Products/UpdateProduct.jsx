6ef8c8d8b3ad8e427f17b71ffda38bdff237328d
sandhyac123456 <chouhansandhya614@gmail.com>
2026-03-31 20:40:54 +0530

    update login  page

--- src/Products/UpdateProduct.jsx
+++ src/Products/UpdateProduct.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import "./Register.css";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
